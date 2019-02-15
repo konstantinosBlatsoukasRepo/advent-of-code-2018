ef9873c3916aeab72991da8e0207dc498b4f09bd
Konstantinos Blatsoukas <konstantinosBlatsoukasRepo@gmail.com>
2019-02-15 16:49:08 +0200

    refactor(day_three_pt1)

---
+++ lib/day_three/claims_parser.ex
@@ -0,0 +1,39 @@
+defmodule ClaimsParser do
+  @day_three_input_path "./lib/day_three/day_three_input.txt"
+  # @day_three_input_path "./lib/day_three/test_input.txt"
+
+  def parse_claims do
+    AdventOfCode2018.read_input(@day_three_input_path)
+    |> Enum.map(&parse_claim/1)
+  end
+
+  def parse_claim(claim) do
+    [_, coordinates] = String.split(claim, "@")
+    [dist_from_edges, dimensions] = String.split(coordinates, ":")
+
+    {dist_from_left, dist_from_top} = parse_distances(dist_from_edges)
+
+    {width, height} = parse_dimensions(dimensions)
+
+    left_upper_corner = {dist_from_left, dist_from_top}
+    bottom_right_corner = {dist_from_left + (width - 1), dist_from_top + (height - 1)}
+
+    {left_upper_corner, bottom_right_corner}
+  end
+
+  defp parse_distances(dist_from_edges) do
+    [dist_left, dist_top] =
+      String.split(dist_from_edges, ",")
+      |> Enum.map(&String.trim/1)
+
+    {String.to_integer(dist_left), String.to_integer(dist_top)}
+  end
+
+  defp parse_dimensions(dimensions) do
+    [width, height] =
+      String.split(dimensions, "x")
+      |> Enum.map(&String.trim/1)
+
+    {String.to_integer(width), String.to_integer(height)}
+  end
+end
--- lib/day_three/day_three.ex
+++ lib/day_three/day_three.ex
@@ -1,45 +1,6 @@
 defmodule DayThree do
-  @day_three_input_path "./lib/day_three/day_three_input.txt"
-
-  # @day_three_input_path "./lib/day_three/test_input.txt"
-
-  def parse_claims do
-    AdventOfCode2018.read_input(@day_three_input_path)
-    |> Enum.map(&parse_claim/1)
-  end
-
-  def parse_claim(claim) do
-    [_, coordinates] = String.split(claim, "@")
-    [dist_from_edges, dimensions] = String.split(coordinates, ":")
-
-    {dist_from_left, dist_from_top} = parse_distances(dist_from_edges)
-
-    {width, height} = parse_dimensions(dimensions)
-
-    left_upper_corner = {dist_from_left, dist_from_top}
-    bottom_right_corner = {dist_from_left + (width - 1), dist_from_top + (height - 1)}
-
-    {left_upper_corner, bottom_right_corner}
-  end
-
-  defp parse_distances(dist_from_edges) do
-    [dist_left, dist_top] =
-      String.split(dist_from_edges, ",")
-      |> Enum.map(&String.trim/1)
-
-    {String.to_integer(dist_left), String.to_integer(dist_top)}
-  end
-
-  defp parse_dimensions(dimensions) do
-    [width, height] =
-      String.split(dimensions, "x")
-      |> Enum.map(&String.trim/1)
-
-    {String.to_integer(width), String.to_integer(height)}
-  end
-
   def calculate_common_inches do
-    [first_claim | rest] = parse_claims()
+    [first_claim | rest] = ClaimsParser.parse_claims()
 
     common_inches = do_calculate_common_inches(first_claim, rest, MapSet.new(), rest)
     MapSet.size(common_inches)
@@ -58,7 +19,8 @@ defmodule DayThree do
     current_claim_points = generate_claim_points(current_claim)
     next_to_current_claim_points = generate_claim_points(next_to_current)
 
-    common_inches = extract_common_inches(current_claim_points, next_to_current_claim_points, map_set)
+    common_inches =
+      extract_common_inches(current_claim_points, next_to_current_claim_points, map_set)
 
     do_calculate_common_inches(
       current_claim,
