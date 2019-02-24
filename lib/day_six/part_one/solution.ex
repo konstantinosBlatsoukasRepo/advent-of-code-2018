cb07d2cc9f903ec2021f1f177809003465a996ed
kostas <konstantinosblatsoukasrepo@gmail.com>
2019-02-24 19:54:57 +0200

    add(day_six_pt1)

--- lib/day_six/part_one/solution.ex
+++ lib/day_six/part_one/solution.ex
@@ -2,11 +2,22 @@ defmodule DaySix.PartOne.Solution do
   alias DaySix.Point
 
   def part_one() do
-    points = DaySix.Parser.parse_points()
+    parsed_points =
+      DaySix.Parser.parse_points()
+      |> MapSet.new()
 
-    {max_column, max_row} = find_max_row_column(points)
+    {max_column, max_row} = find_max_row_column(parsed_points)
 
-    max_point = %Point{column: max_column, row: max_row}
+    # calculate_closest_point_from(%Point{column: 8, row: 8}, parsed_points)
+
+    points_with_min_distances =
+      generate_all_points({max_column, max_row}, parsed_points)
+      |> Enum.map(fn point ->
+        %Point{column: column, row: row} = point
+        %{{column, row} => calculate_closest_point_from(point, parsed_points)}
+      end)
+
+    extract_infinite_points(points_with_min_distances, {max_column, max_row})
   end
 
   def find_max_row_column(points) do
@@ -28,10 +39,48 @@ defmodule DaySix.PartOne.Solution do
     end
   end
 
+  def generate_all_points(max_row_column, parsed_points) do
+    {max_column, max_row} = max_row_column
+
+    for column <- 0..max_column,
+        row <- 0..max_row,
+        not MapSet.member?(parsed_points, %Point{column: column, row: row}) do
+      %Point{column: column, row: row}
+    end
+  end
+
+  def calculate_closest_point_from(point, parsed_points) do
+    Enum.reduce(
+      parsed_points,
+      {%Point{}, 1_000_000_000_000_000_000},
+      fn parsed_point, {min_point, min_distance} ->
+        current_distance = manhattan_distance(point, parsed_point)
+
+        cond do
+          # current_distance == min_distance -> {".", 0}
+          current_distance < min_distance -> {parsed_point, current_distance}
+          true -> {min_point, min_distance}
+        end
+      end
+    )
+  end
+
   def manhattan_distance(point, another_point) do
     %Point{column: column, row: row} = point
     %Point{column: another_column, row: another_row} = another_point
 
     abs(column - another_column) + abs(row - another_row)
+  end
+
+  def extract_infinite_points(points_with_min_distances, {max_column, max_row}) do
+    Enum.reduce(points_with_min_distances, MapSet.new(), fn map, acc ->
+      [{column, row}] = Map.keys(map)
+      [{point, _}] = Map.values(map)
+
+      case column == 0 or row == 0 or column == max_column or row == max_row do
+        true -> MapSet.put(acc, point)
+        false -> acc
+      end
+    end)
   end
 end
