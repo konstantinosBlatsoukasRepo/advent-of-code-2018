3ebf226aa7a7be8881f70f8e674d9293e62eda0a
Konstantinos Blatsoukas <k.blatsoukas@stoiximan.com>
2019-03-06 10:25:15 +0200

    add(day_seven_pt2)

---
+++ lib/day_seven/part_two/costs_calculator.ex
@@ -0,0 +1,22 @@
+defmodule DaySeven.PartTwo.CostsCalculator do
+  @last_step_additional_cost 25
+  @first_step_cost 1
+  # @first_step_cost 60
+
+
+  def initialize_steps_costs() do
+    last_step_cost = @first_step_cost + @last_step_additional_cost
+    letter_values = Enum.map(@first_step_cost..last_step_cost, & &1)
+
+    Enum.map(?A..?Z, &to_string([&1]))
+    |> Enum.zip(letter_values)
+    |> convert_to_map()
+  end
+
+  defp convert_to_map(letter_value_pairs) do
+    Enum.reduce(letter_value_pairs, %{}, fn letter_value_pair, acc ->
+      {letter, value} = letter_value_pair
+      Map.put(acc, letter, value)
+    end)
+  end
+end
--- lib/day_seven/part_two/solution.ex
+++ lib/day_seven/part_two/solution.ex
@@ -1,30 +1,49 @@
 defmodule DaySeven.PartTwo.Solution do
-  # each leter takes the value of the capital letter -4
-
-  @last_step_additional_cost 25
+  alias DaySeven.PartOne.GraphBuilder
   alias DaySeven.PartOne.Solution, as: PartOne
 
-  def part_two(first_step_cost, total_workers) do
+  alias DaySeven.PartTwo.CostsCalculator
 
-    [first_completed | rest] = PartOne.calculate_the_starting_steps()
+  @graph GraphBuilder.build_steps_graph()
+  @initial_steps_costs CostsCalculator.initialize_steps_costs()
+  @total_workers 2
 
-    initial_steps_costs = initialize_steps_costs(first_step_cost)
+  def part_two() do
+    available = PartOne.calculate_the_starting_steps()
+    do_part_two(available, [], 0, @initial_steps_costs)
+  end
+
+  def do_part_two(available, completed, total_completed, current_costs) do
+    steps_for_reduction = Enum.take(available, @total_workers)
+
+    updated_costs = decrease_work_amount(steps_for_reduction, current_costs)
+
+    completed_steps = extract_completed_steps(updated_costs)
+
+    total_completed = length(completed_steps)
+
 
   end
 
-  def initialize_steps_costs(first_step_cost) do
-    last_step_cost = first_step_cost + @last_step_additional_cost
-    letter_values = Enum.map(first_step_cost..last_step_cost, & &1)
+  def decrease_work_amount([], current_costs), do: current_costs
 
-    Enum.map(?A..?Z, &to_string([&1]))
-    |> Enum.zip(letter_values)
-    |> convert_to_map()
+  def decrease_work_amount(steps_for_reduction, current_costs) do
+    [current_step | rest] = steps_for_reduction
+
+    current_value = Map.get(current_costs, current_step)
+
+    updated_costs = Map.put(current_costs, current_step, current_value - 1)
+
+    decrease_work_amount(rest, updated_costs)
   end
 
-  defp convert_to_map(letter_value_pairs) do
-    Enum.reduce(letter_value_pairs, %{}, fn letter_value_pair, acc ->
-      {letter, value} = letter_value_pair
-      Map.put(acc, letter, value)
+  def extract_completed_steps(updated_costs) do
+    Enum.reduce(updated_costs, [], fn {k, v}, acc ->
+      case v == 0 do
+        true -> [k | acc]
+        false -> acc
+      end
     end)
+    |> Enum.sort()
   end
 end
